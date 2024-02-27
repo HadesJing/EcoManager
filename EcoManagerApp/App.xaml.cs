fd2c5eb1719173cd4c1a5d7446f0e8b91373fc01
王晶 <441456@qq.com>
2024-02-27 13:25:49 +0800

    新建APP项目

---
+++ EcoManagerApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EcoManagerApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
